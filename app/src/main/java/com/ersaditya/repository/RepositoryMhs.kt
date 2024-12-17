01ab905793c324094e2577e3f4e2d5433cf6752f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-12-17 15:13:43 +0700

    Menambah Operasi Read, Update, dan Delete pada RepositoryMhs.kt

--- app/src/main/java/com/ersaditya/repository/RepositoryMhs.kt
+++ app/src/main/java/com/ersaditya/repository/RepositoryMhs.kt
@@ -1,8 +1,18 @@
 package com.ersaditya.repository
 
 import com.ersaditya.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 // tiap fungsi di DAO harus ada juga di reposityo
 interface RepositoryMhs
 {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getALlMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
